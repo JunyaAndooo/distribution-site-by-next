c24b2655bb9911240a2d43782835294eac432663
nossa <sa.50.00.no.riku+github@gmail.com>
2020-07-04 18:34:59 +0900

    eslintとprettierを導入

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,59 +1,58 @@
 /** @jsx jsx */
-import { Global, css, jsx } from '@emotion/core';
+import { Global, css, jsx } from "@emotion/core";
 
 /*
  * CSS
  */
 const globalCss = css`
-* {
+  * {
     margin: 0;
     padding: 0;
-}
+  }
 `;
 
 const styles = {
-    titleBase: css`
-        position: absolute;
-        height: 168px;
-        left: 0%;
-        right: 0%;
-        top: 0px;    
-    `,
-    title: css`
-        position: absolute;
-        width: 100%;
-        height: 168px;
-        left: 0px;
-        top: 0px;
-        font-family: Roboto;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 64px;
-        line-height: 75px;
-        display: flex;
-        align-items: center;
-        color: #FFFFFF;
-        background: #2F80ED;
-    `,
+  titleBase: css`
+    position: absolute;
+    height: 168px;
+    left: 0%;
+    right: 0%;
+    top: 0px;
+  `,
+  title: css`
+    position: absolute;
+    width: 100%;
+    height: 168px;
+    left: 0px;
+    top: 0px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 64px;
+    line-height: 75px;
+    display: flex;
+    align-items: center;
+    color: #ffffff;
+    background: #2f80ed;
+  `,
 };
 
 /*
  * Content
  */
 interface Props {
-    title: string;
+  title: string;
 }
 
 const Header = (props: Props) => {
-    
-    return (
-        <>
-            <Global styles={globalCss} />
-            <div css={styles.titleBase}>
-                <h1 css={styles.title}>{props.title}</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Global styles={globalCss} />
+      <div css={styles.titleBase}>
+        <h1 css={styles.title}>{props.title}</h1>
+      </div>
+    </>
+  );
 };
 
 export default Header;
