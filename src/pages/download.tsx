b224b8bef9f242954601b76efc89bb58f9fb06ef
nossa <sa.50.00.no.riku+github@gmail.com>
2020-08-15 02:34:40 +0900

    ダウンロードページ #2

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -2,62 +2,55 @@ import { AuthContext } from "components/common/authProvider";
 import firebase from "firebase";
 import { PageFC } from "next";
 import Router from "next/router";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Tile } from "types/tile";
+
+const getData = async (searchWord: string): Promise<Tile[]> => {
+  const db = firebase.firestore();
+  const tileDataRef = db.collection("tileData");
+  const searchedData = tileDataRef.where(
+    "keyword",
+    "array-contains",
+    searchWord
+  );
+  const snapShot = await searchedData.get();
+  const tiles = snapShot.docs.map((doc) => doc.data() as Tile);
+  return tiles;
+};
 
 type Props = {
   title: string;
-  keyword: string;
+  tiles: Tile[];
 };
 
-const Donwload: PageFC<Props> = (props: Props) => {
-  const [tile, setTile] = useState<Tile | null>(null);
-  const authContext = useContext(AuthContext);
-
-  const getData = async (searchWord: string) => {
-    const db = firebase.firestore();
-    const tileDataRef = db.collection("tileData");
-    const searchedData = tileDataRef.where(
-      "keyword",
-      "array-contains",
-      searchWord
-    );
-    const snapShot = await searchedData.get();
-    const tiles = snapShot.docs.map((doc) => doc.data() as Tile);
-    const tile = tiles.length === 0 ? null : tiles[0];
-
-    setTile(tile);
-  };
-
-  useEffect(() => {
-    if (authContext.user) {
-      getData(props.keyword);
-    } else {
-      Router.push("/");
-    }
-  }, [props.keyword, authContext.user]);
-
-  if (!tile) {
-    return <></>;
-  }
-
+const Download: PageFC<Props> = (props: Props) => {
   return (
-    <div>
-      <img src={tile.image} alt={tile.title} />
-      <h3>{tile.title}</h3>
-
-      <a href={tile.downloadUrl} download>
-        <button>ダウンロードする</button>
-      </a>
-    </div>
+    <>
+      {props.tiles.map((tile) => (
+        <div key={tile.title}>
+          <img src={tile.image} alt={tile.title} />
+          <h3>{tile.title}</h3>
+          <a href={tile.downloadUrl} download>
+            <button>ダウンロードする</button>
+          </a>
+        </div>
+      ))}
+    </>
   );
 };
 
-Donwload.getInitialProps = async ({ query: { keyword } }) => {
+Download.getInitialProps = async ({ query: { keyword } }) => {
+  const authContext = useContext(AuthContext);
+  if (!authContext.authenticated) {
+    Router.push("/");
+  }
+
+  const tiles = await getData(typeof keyword === "string" ? keyword : "");
+
   return {
-    keyword: typeof keyword === "string" ? keyword : "",
     title: "ダウンロード",
+    tiles: tiles,
   };
 };
 
-export default Donwload;
+export default Download;
